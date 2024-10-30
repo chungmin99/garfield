0d49068fb35f37a9f71f97299af6957cac4bf02b
Andrew Goldberg <apgoldberg@gmail.com>
2024-10-30 16:01:34 -0700

    fix gsplat cuda_legacy quat_to_rotmat

--- garfield/garfield_gaussian_pipeline.py
+++ garfield/garfield_gaussian_pipeline.py
@@ -25,13 +25,30 @@ import tqdm
 from sklearn.preprocessing import QuantileTransformer
 from sklearn.neighbors import NearestNeighbors
 
-from gsplat.cuda_legacy._torch_impl import quat_to_rotmat
 from scipy.spatial.transform import Rotation as Rot
 
 from garfield.garfield_datamanager import GarfieldDataManagerConfig, GarfieldDataManager
 from garfield.garfield_model import GarfieldModel, GarfieldModelConfig
 from garfield.garfield_pipeline import GarfieldPipelineConfig, GarfieldPipeline
 
+def quat_to_rotmat(quat):
+    assert quat.shape[-1] == 4, quat.shape
+    w, x, y, z = torch.unbind(quat, dim=-1)
+    mat = torch.stack(
+        [
+            1 - 2 * (y**2 + z**2),
+            2 * (x * y - w * z),
+            2 * (x * z + w * y),
+            2 * (x * y + w * z),
+            1 - 2 * (x**2 + z**2),
+            2 * (y * z - w * x),
+            2 * (x * z - w * y),
+            2 * (y * z + w * x),
+            1 - 2 * (x**2 + y**2),
+        ],
+        dim=-1,
+    )
+    return mat.reshape(quat.shape[:-1] + (3, 3))
 
 def generate_random_colors(N=5000) -> torch.Tensor:
     """Generate random colors for visualization"""
